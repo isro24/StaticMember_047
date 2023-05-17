ebd08b81f4bdc1920c3fd883d31b2c3df737e0de
isro24 <isro.usman.ft22@mail.umy.ac.id>
2023-05-17 18:14:46 +0700

    setjudul dipanggil dan getjudul untuk mengambil nilai judul dari bukunya

--- chainfunction/chainfunction.cpp
+++ chainfunction/chainfunction.cpp
@@ -16,3 +16,11 @@ public:
 	}
 
 } bukunya;
+
+int main()
+{
+	// bukunya.setJudul("Matematika");
+	// cout << bukunya.getJudul();
+	cout << bukunya.setJudul("Matematika").getjudul(); // chain function calls
+	return 0;
+}
